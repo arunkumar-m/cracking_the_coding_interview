c77569420d142c70a23180adb2513f21f3e8a24a
Wei Wei <fraternite@live.cn>
2014-06-20 15:00:20 -0700

    BST finished.

--- GlassDoor/tableau/main.cpp
+++ GlassDoor/tableau/main.cpp
@@ -12,6 +12,22 @@ int main() {
     bst.insert(1);
     cout << "insert 2\n";
     bst.insert(2);
+    cout << "lookup 4\n";
+    for (int i = 0; i < 10; i++) {
+      cout << i << " found? " << bst.lookup(i) << endl;
+    }
+    bst.print();
+    cout << "delete 3\n";
+    bst.remove(3);
+    bst.print();
+    cout << "delete 1\n";
+    bst.remove(1);
+    bst.print();
+    cout << "delete 4\n";
+    bst.remove(4);
+    bst.print();
+    cout << "delete 2\n";
+    bst.remove(2);
     bst.print();
     return 0;
 }
