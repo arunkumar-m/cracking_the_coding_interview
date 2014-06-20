6530457f2b7dc31a73bc40985b8eaa0057ebba05
Wei Wei <fraternite@live.cn>
2014-06-19 21:19:14 -0700

    Balanced Binary Tree finished.

---
+++ LeetCode-Java/src/com/wwei2/leetcode/balancedBinaryTree/Solution.java
@@ -0,0 +1,43 @@
+package com.wwei2.leetcode.balancedBinaryTree;
+
+/**
+ * Created by weiwei on 6/19/14.
+ */
+
+import java.lang.Math;
+import com.wwei2.leetcode.util.TreeNode;
+
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return isBalancedHelper(root, 0) < Integer.MAX_VALUE;
+    }
+
+    public int isBalancedHelper(TreeNode root, int level) {
+        if (root == null) return level;
+        else {
+            int l = isBalancedHelper(root.left, level + 1);
+            int r = isBalancedHelper(root.right, level + 1);
+            if (Math.abs(l - r) > 1) {
+                return Integer.MAX_VALUE;
+            } else {
+                return Math.max(l, r);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t4 = new TreeNode(4, t5, t6);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(2, t3, t4);
+        TreeNode t1 = new TreeNode(1, t2, null);
+        System.out.println(t4);
+        System.out.println("isBalanced: " + sol.isBalanced(t4));
+        System.out.println(t1);
+        System.out.println("isBalanced: " + sol.isBalanced(t1));
+        System.out.println(t2);
+        System.out.println("isBalanced: " + sol.isBalanced(t2));
+    }
+}
---
+++ LeetCode-Java/src/com/wwei2/leetcode/util/TreeNode.java
@@ -0,0 +1,40 @@
+package com.wwei2.leetcode.util;
+
+/**
+ * Created by weiwei on 6/19/14.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+    public TreeNode(int x, TreeNode left, TreeNode right) {
+        this.val = x;
+        this.left = left;
+        this.right = right;
+    }
+
+    public String toString() {
+        if (this == null) return "Empty tree.";
+        StringBuilder sb = new StringBuilder();
+        sb.append(val + ": ");
+        sb.append("{ ");
+        if (left != null) {
+            sb.append(left);
+        } else {
+            sb.append("#");
+        }
+        sb.append(", ");
+        if (right != null) {
+            sb.append(right);
+        } else {
+            sb.append("#");
+        }
+        sb.append(" }");
+        return sb.toString();
+    }
+}
