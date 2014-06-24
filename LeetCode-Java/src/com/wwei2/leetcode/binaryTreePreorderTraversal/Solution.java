c8056531a70ae181de0d648ed519683a6c29ade8
Wei Wei <fraternite@live.cn>
2014-06-23 17:59:53 -0700

    Preorder traversal iterative solution.

--- LeetCode-Java/src/com/wwei2/leetcode/binaryTreePreorderTraversal/Solution.java
+++ LeetCode-Java/src/com/wwei2/leetcode/binaryTreePreorderTraversal/Solution.java
@@ -4,6 +4,7 @@ import com.wwei2.leetcode.util.TreeNode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by weiwei on 6/23/14.
@@ -25,6 +26,21 @@ public class Solution {
         }
     }
 
+    public List<Integer> preorderTraversalIterative(TreeNode root) {
+        List<Integer> result = new LinkedList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                result.add(node.val);
+                stack.push(node.right);
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Solution sol = new Solution();
         TreeNode root = new TreeNode(10);
@@ -32,7 +48,7 @@ public class Solution {
         root.right = new TreeNode(15);
         root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(6);
-        List<Integer> result = sol.preorderTraversal(root);
+        List<Integer> result = sol.preorderTraversalIterative(root);
         System.out.print("List: <");
         for (int i: result) {
             System.out.print(i + ",");
