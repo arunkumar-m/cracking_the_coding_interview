419bc75df1e25afa1b3e96f9ca5f1345026a6e19
Wei Wei <fraternite@live.cn>
2014-06-26 00:15:02 -0700

    Minimum depth of binary tree solution added.

---
+++ LeetCode-Java/src/main/java/com/wwei2/leetcode/minimumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,24 @@
+package com.wwei2.leetcode.minimumDepthOfBinaryTree;
+
+import com.wwei2.leetcode.util.TreeNode;
+
+/**
+ * Created by weiwei on 6/26/14.
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        } else {
+            int leftMin = Integer.MAX_VALUE, rightMin = Integer.MAX_VALUE;
+            if (root.left != null)
+                leftMin = minDepth(root.left);
+            if (root.right != null)
+                rightMin = minDepth(root.right);
+            return 1 + Math.min(leftMin, rightMin);
+        }
+    }
+}
---
+++ LeetCode-Java/src/test/java/com/wwei2/leetcode/minimumDepthOfBinaryTree/TestSolution.java
@@ -0,0 +1,41 @@
+package com.wwei2.leetcode.minimumDepthOfBinaryTree;
+
+import com.wwei2.leetcode.util.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by weiwei on 6/26/14.
+ */
+public class TestSolution {
+    private Solution sol = new Solution();
+    @Test
+    public void testSingle() {
+        TreeNode root = new TreeNode(1);
+        Assert.assertEquals(1, sol.minDepth(root));
+    }
+    @Test
+    public void testMultiple1() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(6);
+        root.right = new TreeNode(5);
+        System.out.println(root);
+        Assert.assertEquals(2, sol.minDepth(root));
+    }
+    @Test
+    public void testMultiple2() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(6);
+        root.right = new TreeNode(5);
+        root.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(8);
+        System.out.println(root);
+        Assert.assertEquals(3, sol.minDepth(root));
+    }
+}
