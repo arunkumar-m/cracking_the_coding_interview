e0864b22e26a19e5f8a0b3ab1cc4988d0ad2fa2e
Wei Wei <fraternite@live.cn>
2014-06-29 23:36:10 -0700

    Converted sorted list to bst finished.

---
+++ LeetCode-Java/src/main/java/com/wwei2/leetcode/convertSortedListToBinarySearchTree/Solution.java
@@ -0,0 +1,28 @@
+package com.wwei2.leetcode.convertSortedListToBinarySearchTree;
+
+import com.wwei2.leetcode.util.ListNode;
+import com.wwei2.leetcode.util.ListUtil;
+import com.wwei2.leetcode.util.TreeNode;
+
+/**
+ * Created by weiwei on 6/29/14.
+ */
+public class Solution {
+    private ListNode curr;
+    public TreeNode sortedListToBST(ListNode head) {
+        int n = ListUtil.getLength(head);
+        curr = head;
+        return sortedListToBSTHelper(0, n - 1);
+    }
+    private TreeNode sortedListToBSTHelper(int start, int end) {
+        if (start > end) return null;
+        int mid = start + (end - start) / 2;
+        TreeNode left = sortedListToBSTHelper(start, mid - 1);
+        TreeNode node = new TreeNode(curr.val);
+        curr = curr.next;
+        TreeNode right = sortedListToBSTHelper(mid + 1, end);
+        node.left = left;
+        node.right = right;
+        return node;
+    }
+}
---
+++ LeetCode-Java/src/test/java/com/wwei2/leetcode/convertSortedListToBinarySearchTree/TestSolution.java
@@ -0,0 +1,33 @@
+package com.wwei2.leetcode.convertSortedListToBinarySearchTree;
+
+import com.wwei2.leetcode.util.ListNode;
+import com.wwei2.leetcode.util.TreeNode;
+import org.junit.Test;
+
+/**
+ * Created by weiwei on 6/29/14.
+ */
+public class TestSolution {
+    private Solution sol = new Solution();
+    @Test
+    public void testSmallList() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        TreeNode root = sol.sortedListToBST(head);
+        System.out.println(root);
+    }
+
+    @Test
+    public void testMidList() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        TreeNode root = sol.sortedListToBST(head);
+        System.out.println(root);
+    }
+}
