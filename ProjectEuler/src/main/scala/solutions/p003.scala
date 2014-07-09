4b766fecfc63f9cff30346c63c56941a5595e503
Wei Wei <fraternite@live.cn>
2014-07-08 22:02:28 -0700

    [PE] p003 finished.

---
+++ ProjectEuler/src/main/scala/solutions/p003.scala
@@ -0,0 +1,27 @@
+package solutions
+
+/** Largest prime factor
+ *  
+ *  The prime factors of 13195 are 5, 7, 13, 29.
+ *  
+ *  What is the largest prime factor of the number 600851475143?
+ *  
+ */
+
+object p003 {
+  
+  def sieve(x: Int): Seq[Int] = {
+    val bound = math.sqrt(x)
+    def loop(xs: Seq[Int]): Seq[Int] = xs match {
+      case y +: ys => if (y <= bound) y +: loop(ys filter (_ % y != 0)) else xs
+    }
+    loop(2 to x)
+  }
+  
+  def main(args: Array[String]): Unit = {
+    val num = 600851475143L
+    val primes = sieve(math.sqrt(num.toDouble).toInt)
+    println("Largest prime factor: " + (primes filter (num % _ == 0)).max)
+  }
+
+}
