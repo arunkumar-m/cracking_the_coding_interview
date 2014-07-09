44563e8e156a2fc0351538a75c092713823a2405
Wei Wei <fraternite@live.cn>
2014-07-09 11:28:18 -0700

    [PE] update document style.

--- ProjectEuler/src/main/scala/solutions/p003.scala
+++ ProjectEuler/src/main/scala/solutions/p003.scala
@@ -1,15 +1,16 @@
 package solutions
 
-/** Largest prime factor
- *  
- *  The prime factors of 13195 are 5, 7, 13, 29.
- *  
- *  What is the largest prime factor of the number 600851475143?
- *  
+/**
+ * Largest prime factor
+ *
+ * The prime factors of 13195 are 5, 7, 13, 29.
+ *
+ * What is the largest prime factor of the number 600851475143?
+ *
  */
 
 object p003 {
-  
+
   def sieve(x: Int): Seq[Int] = {
     val bound = math.sqrt(x)
     def loop(xs: Seq[Int]): Seq[Int] = xs match {
@@ -17,7 +18,7 @@ object p003 {
     }
     loop(2 to x)
   }
-  
+
   def main(args: Array[String]): Unit = {
     val num = 600851475143L
     val primes = sieve(math.sqrt(num.toDouble).toInt)
--- ProjectEuler/src/main/scala/solutions/p004.scala
+++ ProjectEuler/src/main/scala/solutions/p004.scala
@@ -1,36 +1,37 @@
 package solutions
 
-/** Largest palindrome product
- *  
- *  A palindromic number reads the same both ways. The largest palindrome
- *  made from the product of two 2-digit numbers is 9009 = 91 × 99.
- *  
- *  Find the largest palindrome made from the product of two 3-digit numbers.
- *  
+/**
+ * Largest palindrome product
+ *
+ * A palindromic number reads the same both ways. The largest palindrome
+ * made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ *
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ *
  */
 
 object p004 {
-  
+
   // All palindromes with 5 digits.
   lazy val digits5 = {
     for (x <- 1 to 9; y <- 0 to 9; z <- 0 to 9)
       yield (x.toString + y.toString + z.toString +y.toString +
           x.toString).toInt
   }
-  
+
   // All palindromes with 6 digits.
   lazy val digits6 = {
     for (x <- 1 to 9; y <- 0 to 9; z <- 0 to 9)
       yield (x.toString + y.toString + z.toString + z.toString +
           y.toString + x.toString).toInt
   }
-  
+
   // All numbers that is the product of two 3-digit numbers.
   lazy val candidates = {
     for (x <- 100 to 999; y <- 100 to 999)
       yield x * y
   }
-    
+
 
   def main(args: Array[String]): Unit = {
     println(((digits5 ++ digits6) intersect candidates).max)
