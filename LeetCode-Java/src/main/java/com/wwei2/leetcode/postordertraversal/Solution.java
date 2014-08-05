6095682511d9b82335ec9e7fa82f46ab536ccecb
Wei Wei <wwei2@outlook.com>
2014-08-04 23:04:17 -0700

    [LC] Added three states solution to postorder traversal.

--- LeetCode-Java/src/main/java/com/wwei2/leetcode/postordertraversal/Solution.java
+++ LeetCode-Java/src/main/java/com/wwei2/leetcode/postordertraversal/Solution.java
@@ -1,5 +1,6 @@
 package com.wwei2.leetcode.postordertraversal;
 
+import com.wwei2.leetcode.util.Pair;
 import com.wwei2.leetcode.util.TreeNode;
 
 import java.util.Collections;
@@ -12,42 +13,82 @@ import java.util.Stack;
  */
 public class Solution {
 
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list = new LinkedList<Integer>();
-        TreeNode prev = new TreeNode(0); // Dummy
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.add(root);
-        while (!stack.empty()) {
-            TreeNode node = stack.pop();
-            if (node != null) {
-                if ((node.left == null && node.right == null) ||
-                        (node.right == null && prev == node.left) ||
-                        prev == node.right) {
-                    list.add(node.val);
-                } else {
-                    stack.push(node);
-                    stack.push(node.right);
-                    stack.push(node.left);
-                }
-                prev = node;
-            }
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> list = new LinkedList<Integer>();
+    TreeNode prev = new TreeNode(0); // Dummy
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    stack.add(root);
+    while (!stack.empty()) {
+      TreeNode node = stack.pop();
+      if (node != null) {
+        if ((node.left == null && node.right == null) ||
+            (node.right == null && prev == node.left) ||
+            prev == node.right) {
+          list.add(node.val);
+          prev = node;
+        } else {
+          stack.push(node);
+          stack.push(node.right);
+          stack.push(node.left);
         }
-        return list;
+      }
     }
+    return list;
+  }
 
-    public List<Integer> postorderTraversalTwoStack(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        List<Integer> result = new LinkedList<Integer>();
-        stack.push(root);
-        while (!stack.empty()) {
-            TreeNode node = stack.pop();
-            if (node != null) {
-                stack.push(node.left);
-                stack.push(node.right);
-                result.add(node.val);
-            }
-        }
-        Collections.reverse(result);
-        return result;
+  private enum Type {
+    FIRST,
+    SECOND,
+    THIRD
+  }
+
+  public List<Integer> postorderTraversalTwoState(TreeNode root) {
+    List<Integer> results = new LinkedList<>();
+    Stack<Pair<TreeNode, Type>> stack = new Stack<>();
+    if (root != null) {
+      stack.push(new Pair<TreeNode, Type>(root, Type.FIRST));
     }
+    while (!stack.isEmpty()) {
+      Pair<TreeNode, Type> pair = stack.pop();
+      TreeNode node = pair.getFirst();
+      Type type = pair.getSecond();
+      if (node != null) {
+        switch (type) {
+          case FIRST:
+            stack.push(new Pair<TreeNode, Type>(node, Type.SECOND));
+            if (node.left != null) {
+              stack.push(new Pair<TreeNode, Type>(node.left, Type.FIRST));
+            }
+            break;
+          case SECOND:
+            stack.push(new Pair<TreeNode, Type>(node, Type.THIRD));
+            if (node.right != null) {
+              stack.push(new Pair<TreeNode, Type>(node.right, Type.FIRST));
+            }
+            break;
+          case THIRD:
+            results.add(node.val);
+            break;
+        }
+      }
+    }
+    return results;
+  }
+
+  // Use two stacks to solve postorder traversal.
+  public List<Integer> postorderTraversalTwoStack(TreeNode root) {
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    List<Integer> result = new LinkedList<Integer>();
+    stack.push(root);
+    while (!stack.empty()) {
+      TreeNode node = stack.pop();
+      if (node != null) {
+        stack.push(node.left);
+        stack.push(node.right);
+        result.add(node.val);
+      }
+    }
+    Collections.reverse(result);
+    return result;
+  }
 }
--- LeetCode-Java/src/test/java/com/wwei2/leetcode/postordertraversal/TestSolution.java
+++ LeetCode-Java/src/test/java/com/wwei2/leetcode/postordertraversal/TestSolution.java
@@ -12,42 +12,49 @@ import java.util.List;
  * Created by weiwei on 6/24/14.
  */
 public class TestSolution {
-    static private Solution sol = new Solution();
-    static TreeNode root1;
-    static TreeNode root2;
+  static private Solution sol = new Solution();
+  static TreeNode root1;
+  static TreeNode root2;
 
-    @BeforeClass
-    public static void beforeClass() {
-        root1 = new TreeNode(5);
-        root1.left = new TreeNode(1);
+  @BeforeClass
+  public static void beforeClass() {
+    root1 = new TreeNode(5);
+    root1.left = new TreeNode(1);
 
-        root2 = new TreeNode(5);
-        root2.left = new TreeNode(3);
-        root2.right = new TreeNode(10);
-        root2.left.left = new TreeNode(1);
-        root2.left.right = new TreeNode(4);
-        root2.right.left = new TreeNode(7);
-        root2.right.right = new TreeNode(15);
-    }
+    root2 = new TreeNode(5);
+    root2.left = new TreeNode(3);
+    root2.right = new TreeNode(10);
+    root2.left.left = new TreeNode(1);
+    root2.left.right = new TreeNode(4);
+    root2.right.left = new TreeNode(7);
+    root2.right.right = new TreeNode(15);
+  }
 
-    @Test
-    public void testTwoNodes() {
-        List<Integer> list = sol.postorderTraversal(root1);
-        System.out.println(list);
-        Assert.assertTrue(list.equals(Arrays.asList(1, 5)));
-    }
+  @Test
+  public void testTwoNodes() {
+    List<Integer> list = sol.postorderTraversal(root1);
+    System.out.println(list);
+    Assert.assertTrue(list.equals(Arrays.asList(1, 5)));
+  }
 
-    @Test
-    public void testSmallTree() {
-        List<Integer> list = sol.postorderTraversal(root2);
-        System.out.println(list);
-        Assert.assertTrue(list.equals(Arrays.asList(1, 4, 3, 7, 15, 10, 5)));
-    }
+  @Test
+  public void testSmallTree() {
+    List<Integer> list = sol.postorderTraversal(root2);
+    System.out.println(list);
+    Assert.assertTrue(list.equals(Arrays.asList(1, 4, 3, 7, 15, 10, 5)));
+  }
 
-    @Test
-    public void testTwoStackSolution() {
-        List<Integer> list = sol.postorderTraversalTwoStack(root2);
-        System.out.println(list);
-        Assert.assertTrue(list.equals(Arrays.asList(1, 4, 3, 7, 15, 10, 5)));
-    }
+  @Test
+  public void testTwoStackSolution() {
+    List<Integer> list = sol.postorderTraversalTwoStack(root2);
+    System.out.println(list);
+    Assert.assertTrue(list.equals(Arrays.asList(1, 4, 3, 7, 15, 10, 5)));
+  }
+
+  @Test
+  public void testTwoStateSolution() {
+    List<Integer> list = sol.postorderTraversalTwoState(root2);
+    System.out.println(list);
+    Assert.assertTrue(list.equals(Arrays.asList(1, 4, 3, 7, 15, 10, 5)));
+  }
 }
