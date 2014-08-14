801c306cc2590a91037252a809f86f75881427b2
Wei Wei <wwei2@outlook.com>
2014-08-13 23:26:11 -0700

    [LC] Updated ListUtil to easily create linked list from a list of integers.

--- LeetCode-Java/src/main/java/com/wwei2/leetcode/util/ListUtil.java
+++ LeetCode-Java/src/main/java/com/wwei2/leetcode/util/ListUtil.java
@@ -1,28 +1,40 @@
 package com.wwei2.leetcode.util;
 
+import java.util.List;
+
 /**
  * Created by weiwei on 6/29/14.
  */
 public class ListUtil {
 
-    public static ListNode getMiddleElem(ListNode head) {
-        ListNode fast = head, slow = head;
-        boolean flip = true;
-        while (fast != null) {
-            fast = fast.next;
-            flip = !flip;
-            if (flip && fast != null)
-                slow = slow.next;
-        }
-        return slow;
+  public static ListNode getMiddleElem(ListNode head) {
+    ListNode fast = head, slow = head;
+    boolean flip = true;
+    while (fast != null) {
+      fast = fast.next;
+      flip = !flip;
+      if (flip && fast != null)
+        slow = slow.next;
     }
+    return slow;
+  }
 
-    public static int getLength(ListNode head) {
-        int len = 0;
-        while (head != null) {
-            head = head.next;
-            len++;
-        }
-        return len;
+  public static int getLength(ListNode head) {
+    int len = 0;
+    while (head != null) {
+      head = head.next;
+      len++;
     }
+    return len;
+  }
+
+  public static ListNode fromList(List<Integer> list) {
+    ListNode head = new ListNode(0); // Dummy.
+    ListNode curr = head;
+    for (int i : list) {
+      curr.next = new ListNode(i);
+      curr = curr.next;
+    }
+    return head.next;
+  }
 }
--- LeetCode-Java/src/test/java/com/wwei2/leetcode/util/TestListUtil.java
+++ LeetCode-Java/src/test/java/com/wwei2/leetcode/util/TestListUtil.java
@@ -3,33 +3,46 @@ package com.wwei2.leetcode.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * Created by weiwei on 6/29/14.
  */
 public class TestListUtil {
-    @Test
-    public void testNull() {
-        ListNode head = null;
-        Assert.assertEquals(null, ListUtil.getMiddleElem(head));
-    }
-    @Test
-    public void testOneElem() {
-        ListNode head = new ListNode(1);
-        Assert.assertEquals(1, ListUtil.getMiddleElem(head).val);
-    }
+  @Test
+  public void testNull() {
+    ListNode head = null;
+    Assert.assertEquals(null, ListUtil.getMiddleElem(head));
+  }
+  @Test
+  public void testOneElem() {
+    ListNode head = new ListNode(1);
+    Assert.assertEquals(1, ListUtil.getMiddleElem(head).val);
+  }
 
-    @Test
-    public void testTwoElem() {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        Assert.assertEquals(1, ListUtil.getMiddleElem(head).val);
-    }
+  @Test
+  public void testTwoElem() {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    Assert.assertEquals(1, ListUtil.getMiddleElem(head).val);
+  }
 
-    @Test
-    public void testThreeElem() {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        Assert.assertEquals(2, ListUtil.getMiddleElem(head).val);
-    }
+  @Test
+  public void testThreeElem() {
+    ListNode head = new ListNode(1);
+    head.next = new ListNode(2);
+    head.next.next = new ListNode(3);
+    Assert.assertEquals(2, ListUtil.getMiddleElem(head).val);
+  }
+
+  @Test
+  public void testFromList() {
+    ListNode head = ListUtil.fromList(Arrays.asList(1, 1, 2, 2, 3));
+    Assert.assertEquals(1, head.val);
+    Assert.assertEquals(1, head.next.val);
+    Assert.assertEquals(2, head.next.next.val);
+    Assert.assertEquals(2, head.next.next.next.val);
+    Assert.assertEquals(3, head.next.next.next.next.val);
+    Assert.assertEquals(null, head.next.next.next.next.next);
+  }
 }
