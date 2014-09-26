087a42b29a8a3f9fba0ba9395d17df3de02c01dd
Wei Wei <wwei2@outlook.com>
2014-09-26 00:35:48 -0700

    [LC] Added solution to climbing stairs.

---
+++ LeetCode-Java/src/main/java/com/wwei2/leetcode/climbingstairs/Solution.java
@@ -0,0 +1,23 @@
+package com.wwei2.leetcode.climbingstairs;
+
+/**
+ * Climbing stairs.
+ */
+public class Solution {
+  public int climbStairs(int n) {
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    } else if (n == 2) {
+      return 2;
+    }
+    int[] dp = new int[n];
+    dp[0] = 1;
+    dp[1] = 2;
+    for (int i = 2; i < n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n - 1];
+  }
+}
